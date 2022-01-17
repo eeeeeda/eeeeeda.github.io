78327855bac6382c99e62a07a8dee29b54d17237
eda0107 <ekdus5530@naver.com>
2022-01-17 20:36:35 +0900

    commit test

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `eeeeeda.github.io`,
-  description: `Leeda`,
+  description: `Leeda의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://eeeeeda.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -15,7 +15,7 @@ module.exports = {
     bio: {
       role: `개발자`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      thumbnail: 'profile.jpg', // Path to the image in the 'asset' folder
     },
     social: {
       github: `https://github.com/eeeeeda`, // `https://github.com/eeeeeda`,
@@ -30,8 +30,8 @@ module.exports = {
       // =====       [Timestamp Sample and Structure]      =====
       // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
       {
-        date: '',
-        activity: '',
+        date: '2021.12 ~',
+        activity: '개인 블로그 개발 및 운영',
         links: {
           github: '',
           post: '',
@@ -42,24 +42,15 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        date: '2021.12~',
-        activity: '개인 블로그 개발 및 운영',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
     ],
 
     projects: [
       // =====        [Project Sample and Structure]        =====
       // ===== 🚫 Don't erase this sample (여기 지우지 마세요!)  =====
       {
-        title: '',
-        description: '',
-        techStack: ['', ''],
+        title: '개발 블로그',
+        description: '개츠비 블로그 테마를 이용하여 개발 기술 블로그를 만들게 되었습니다.',
+        techStack: ['gatsby', 'react'],
         thumbnailUrl: '',
         links: {
           post: '',
@@ -71,18 +62,6 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
-    ],
+     ],
   },
 };
